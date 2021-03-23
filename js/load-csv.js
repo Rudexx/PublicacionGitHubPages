edf1dd8fa93e3f747fe6cb31373a02282a86b81b
unknown <davidnogalesjoseleon@gmail.com>
2021-03-22 23:13:25 -0500

    F

--- js/load-csv.js
+++ js/load-csv.js
@@ -1,6 +1,6 @@
 var datos = undefined;
 // Loading a CSV using D3
-d3.dsv( ";", "../data/pets-citizens.csv" )
+d3.dsv( ";", "pets-citizens.csv" )
   .then( function( data ) {
     datos = data;
   } )
